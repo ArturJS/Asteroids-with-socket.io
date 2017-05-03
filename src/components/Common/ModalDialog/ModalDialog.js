98163c53fffe97c162bae09913714e970f39797c
ArturN <nizamutdin.art@yandex.ru>
2017-05-03 15:39:03 +0400

    Added animations (on appear and disappear) for Modal Dialog

--- src/components/Common/ModalDialog/ModalDialog.js
+++ src/components/Common/ModalDialog/ModalDialog.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {observer, inject} from 'mobx-react';
 import Modal from 'react-modal';
 import _ from 'lodash';
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 import {MODAL_TYPES} from './ModalStore';
 import './ModalDialog.scss';
 
@@ -35,51 +36,64 @@ export default class ModalDialog extends Component {
       <Modal
         isOpen={isOpen}
         onRequestClose={this.dismiss}
+        closeTimeoutMS={250}
         style={noBackdrop ? noBackdropStyle : {}}
         className={`modal ${modalClassName}`}
         contentLabel={''}>
         <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <button
-                type="button"
-                className="close"
-                onClick={this.dismiss}>
-                &times;
-              </button>
-              <h3 className="modal-title">
-                {title}
-              </h3>
-            </div>
-            {modalType === MODAL_TYPES.custom &&
-            <div className="modal-custom-body">
-              {body}
-            </div>
-            }
-            {modalType !== MODAL_TYPES.custom &&
-            <div>
-              <div className="modal-body">
-                {_.isArray(body) ? body.map(item => <p key={item}>{item}</p>) : body}
-              </div>
-              <div className="modal-footer">
+          <CSSTransitionGroup
+            className="fade-in-out"
+            transitionName="fade-in-out"
+            transitionEnter={true}
+            transitionLeave={true}
+            transitionAppear={true}
+            transitionAppearTimeout={350}
+            transitionEnterTimeout={350}
+            transitionLeaveTimeout={350}>
+            {isOpen &&
+            <div className="modal-content">
+              <div className="modal-header">
                 <button
-                  className="btn btn-primary"
                   type="button"
-                  onClick={this.close}>
-                  OK
-                </button>
-                {modalType === MODAL_TYPES.confirm &&
-                <button
-                  className="btn btn-default"
-                  type="button"
+                  className="close"
                   onClick={this.dismiss}>
-                  Cancel
+                  &times;
                 </button>
-                }
+                <h3 className="modal-title">
+                  {title}
+                </h3>
               </div>
+              {modalType === MODAL_TYPES.custom &&
+              <div className="modal-custom-body">
+                {body}
+              </div>
+              }
+              {modalType !== MODAL_TYPES.custom &&
+              <div>
+                <div className="modal-body">
+                  {_.isArray(body) ? body.map(item => <p key={item}>{item}</p>) : body}
+                </div>
+                <div className="modal-footer">
+                  <button
+                    className="btn btn-primary"
+                    type="button"
+                    onClick={this.close}>
+                    OK
+                  </button>
+                  {modalType === MODAL_TYPES.confirm &&
+                  <button
+                    className="btn btn-default"
+                    type="button"
+                    onClick={this.dismiss}>
+                    Cancel
+                  </button>
+                  }
+                </div>
+              </div>
+              }
             </div>
             }
-          </div>
+          </CSSTransitionGroup>
         </div>
       </Modal>
     );
