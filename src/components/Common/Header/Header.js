b52f3e786c7a7bdf09770d12d211b6ea7d3430d4
ArturN <nizamutdin.art@yandex.ru>
2017-05-07 12:22:05 +0400

    Added "Back to home" link

--- src/components/Common/Header/Header.js
+++ src/components/Common/Header/Header.js
@@ -1,18 +1,42 @@
 import React, {Component} from 'react';
-import {Link} from 'react-router';
+import {Link, browserHistory} from 'react-router';
 import UserAuthState from '../UserAuthState';
 import './Header.scss';
 
 export default class Header extends Component {
+  state = {
+    isRoomPage: false
+  };
+
+  componentDidMount() {
+    this.unlisten = browserHistory.listen(this.updateActiveStep);
+    this.updateActiveStep();
+  }
+
+  componentWillUnmount() {
+    this.unlisten();
+  }
+
+  updateActiveStep = () => {
+    this.setState({
+      isRoomPage: /room/.test(browserHistory.getCurrentLocation().hash)
+    });
+  };
+
   render() {
+    const {isRoomPage} = this.state;
+
     return (
       <nav className="header navbar navbar-default navbar-fixed-top">
         <div className="navbar-header">
+          {isRoomPage &&
           <Link className="navbar-brand" to="/">
-            <div className="logo">
-              <div className="logo-sizer"/>
-            </div>
+            <i className="fa fa-arrow-left"/>
+            <span className="navbar-brand__text">
+              Back to home
+            </span>
           </Link>
+          }
           <UserAuthState/>
         </div>
       </nav>
